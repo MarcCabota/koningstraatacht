7ffb05abf44a637d4afc80e5a6665d51fcaff320
MarcCabota <marc.ruiz.cabota@gmail.com>
2026-08-24 05:17:22 +0200

    arreglos

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-document.addEventListener('touchstart', () => {}, { passive: true });
-
 const experiences = document.querySelectorAll('.experience');
 
 experiences.forEach(experience => {
 
     experience.addEventListener('touchstart', () => {
+        experiences.forEach(item => {
+            item.classList.remove('touch-active');
+        });
+
         experience.classList.add('touch-active');
     }, { passive: true });
 
